103ce641e4e66419ba14980e88aa2185ddfd55a3
Roman Martin Gil <rmarting@redhat.com>
2023-05-11 20:12:52 +0200

    🔨 Append messages into Red Hat Single Sign-On instance

---
+++ append-messages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Red Hat Single Sign-On
+#export RHSSO_HOME=/path/to/rh-sso-7.6
+RHSSO_HOME=/home/rmarting/RH/rh-sso-7.6
+
+# Iterate over all files in the source directory with .properties extension
+for file in ./src/main/resources/theme/email-code-theme/email/messages/*.properties; do
+    # Get the filename without the path and extension
+    filename=$(basename "$file" .properties)    
+    # Append the contents of the source file to the destination file
+    echo "Append content of $filename into $RHSSO_HOME/themes/base/email/messages/$filename.properties"
+    cat "$file" >> "$RHSSO_HOME/themes/base/email/messages/$filename.properties"
+done
